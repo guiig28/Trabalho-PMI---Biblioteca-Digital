5b9ecdaace29dc97f5db26861a6ab68d710b391a
Guilherme Cardoso <gui.cardoso.28@gmail.com>
2025-11-18 23:37:30 -0300

    Criação do método return_book() e atualização do método rent_book()

--- functions/book.py
+++ functions/book.py
@@ -2,7 +2,7 @@
 from .user import user_data, logged_in_user, update_user
 
 book_data = ("title", "quantity")
-library = [{book_data[0]: "titulo 1", book_data[1]: 3}, {book_data[0]: "titulo 2", book_data[1]: 8}]
+library = [{book_data[0]: "titulo 1", book_data[1]: 3}, {book_data[0]: "titulo 2", book_data[1]: 8}, {book_data[0]: "titulo 3", book_data[1]: 1}]
 
 def add_book_to_lib(title: str, qt: int):
     found_book = False
@@ -37,17 +37,44 @@ def rmv_book_from_lib(title: str, qt: int):
         print("\nLivro não encontrado.")
 
 def rent_book(title):
-    for i in range(len(library)):
-        if title in logged_in_user[user_data[2]]:
-            print("\nLivro ja alugado pelo usuário.")
-        else:
+    if title in logged_in_user[user_data[2]]:
+            print(f"\nLivro {title} ja alugado pelo usuário.")
+    else:
+        found_book = False
+        for i in range(len(library)):
             if library[i][book_data[0]] == title:
                 library[i][book_data[1]] -= 1
 
                 if library[i][book_data[1]] == 0:
                     library.pop(i)     
 
-                logged_in_user[user_data[2]].append(title)    
+                logged_in_user[user_data[2]].append(title)
+
                 update_user() 
-            else:
-                print("\nLivro não disponível na biblioteca.")
+                found_book = True
+                print(f"\nLivro {title} alugado com sucesso.")
+
+                break
+            
+        if not found_book:
+            print(f"\nLivro {title} não disponível na biblioteca.")
+
+def return_book(title):
+    if title in logged_in_user[user_data[2]]:
+        logged_in_user[user_data[2]].remove(title)
+
+        found_book = False
+        for i in range(len(library)):
+            if library[i][book_data[0]] == title:
+                library[i][book_data[1]] += 1
+                found_book = True
+                break
+
+        if not found_book:
+            library.append({book_data[0]: title, book_data[1]: 1})
+
+        print(f"\nLivro {title} devolvido com sucesso.")
+    else:
+        print(f"\nLivro {title} não encontrado.")
+
+        
